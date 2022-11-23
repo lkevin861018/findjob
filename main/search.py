0e96147fc195531635e77b9c6a8172bdaae08e8f
lkevin861018 <kevinliang1018@gmail.com>
2022-11-23 19:03:50 +0800

    'uplaod'

--- main/search.py
+++ main/search.py
@@ -22,6 +22,7 @@ def search104(request):
         #---------------------------------------------------------------------------##
 
         keyword = request.POST['keyword']
+        keyword.encode('utf-8').decode('latin1')
         num = request.POST['num']
         area = request.POST['area']
         url = "https://www.104.com.tw/jobs/search/?keyword=" + \
@@ -29,12 +30,12 @@ def search104(request):
         chrome_options = Options()
         chrome_options.add_argument("--headless")
     # heroku上使用
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
-        browser = webdriver.Chrome(executable_path=os.environ.get(
-            "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-        # browser = webdriver.Chrome(options=chrome_options)
+        # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        # chrome_options.add_argument("--disable-dev-shm-usage")
+        # chrome_options.add_argument("--no-sandbox")
+        # browser = webdriver.Chrome(executable_path=os.environ.get(
+        #     "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        browser = webdriver.Chrome(options=chrome_options)
         browser.implicitly_wait(5)
         browser.get(url)
         urlsource = browser.page_source
@@ -93,10 +94,13 @@ def search104(request):
                        tempEducationListData[i],
                        tempSalaryListData[i],
                        tempHrefListData[i]])
-
-        return render(request, 'search104.html', context={'FTjob': df})
+        res = render(request, 'search104.html', context={'FTjob': df})
+        res.set_cookie('keyword', keyword)
+        return res
     else:
-        return render(request, 'search104.html')
+        res = render(request, 'search104.html')
+        res.set_cookie('keyword', '')
+        return res
 
 
 def search_hahow(request):
