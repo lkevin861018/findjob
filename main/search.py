06eb9eb8b7666de158d40908d787baf687570a0c
lkevin861018 <kevinliang1018@gmail.com>
2022-11-23 19:06:52 +0800

    'uplaod'

--- main/search.py
+++ main/search.py
@@ -30,12 +30,12 @@ def search104(request):
         chrome_options = Options()
         chrome_options.add_argument("--headless")
     # heroku上使用
-        # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        # chrome_options.add_argument("--disable-dev-shm-usage")
-        # chrome_options.add_argument("--no-sandbox")
-        # browser = webdriver.Chrome(executable_path=os.environ.get(
-        #     "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-        browser = webdriver.Chrome(options=chrome_options)
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        browser = webdriver.Chrome(executable_path=os.environ.get(
+            "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        # browser = webdriver.Chrome(options=chrome_options)
         browser.implicitly_wait(5)
         browser.get(url)
         urlsource = browser.page_source
